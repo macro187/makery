f54e7b725d0e6204c136e496f06abf661a5df0df
Ron MacNeil <macro@hotmail.com>
2010-08-29 05:07:15 +0000

    traitor: New 'traitor' module to support the Traitor C# traits preprocessor

---
+++ traitor/rules.mk
@@ -0,0 +1,48 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2010
+# Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# ------------------------------------------------------------------------------
+
+
+RULE_TARGET := $(TRAITOR_preq)
+RULE_REQS := $(TRAITOR_srcpreq)
+RULE_REQS += $(call PROJ_GetVar,RUNNABLE_reqs,$(TRAITOR_PROJ))
+RULE_OREQ := $(TRAITOR_dir)
+
+
+define RULE_COMMANDS
+	@echo ""
+	@echo "=> Cleaning old output files..."
+	rm -rf $$(call SHELL_Escape,$$(TRAITOR_dir))/*
+	@echo "=> ...done"
+	@echo ""
+	@echo "=> Making subdirs..."
+	$$(foreach dir,$$(TRAITOR_subdirs),$$(MAKE_CHAR_NEWLINE)	mkdir -p $$(TRAITOR_dir)/$$(call MAKE_DecodeWord,$$(dir)))
+	@echo "=> ...done"
+	@echo ""
+	@echo "=> Running Traitor..."
+	$(call PROJ_GetVar,RUNNABLE_run,$(TRAITOR_PROJ)) $(MAKE_CHAR_BS)
+	$(call SHELL_Escape,$(TRAITOR_srcdir)) $(MAKE_CHAR_BS)
+	$$(foreach src,$$(TRAITOR_srcs),$$(call PROJ_RuleNewLine,$$(call SHELL_Escape,$$(call MAKE_DecodeWord,$$(src))))) $(MAKE_CHAR_BS)
+	$(call SHELL_Escape,$(TRAITOR_dir))
+	@echo "=> ...done"
+	@echo ""
+	@echo "=> Updating dotfile..."
+	touch $$(TRAITOR_preq)
+	@echo "=> ...done"
+endef
+
+$(call PROJ_Rule)
+
