0dbaf0e548093d9bab00581395a5ba36839c6943
Ron MacNeil <macro@hotmail.com>
2011-05-26 06:43:56 +0000

    Update copyright years

--- make/global.mk
+++ make/global.mk
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright (c) 2007, 2008, 2009
+# Copyright (c) 2007, 2008, 2009, 2010, 2011
 # Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
 #
 # Permission to use, copy, modify, and distribute this software for any
--- traitor/requires.mk
+++ traitor/requires.mk
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright (c) 2010
+# Copyright (c) 2010, 2011
 # Ron MacNeil <macro187 AT users DOT sourceforge DOT net>
 #
 # Permission to use, copy, modify, and distribute this software for any
