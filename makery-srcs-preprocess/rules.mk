aa4341af4b298c37f6279d6df16c8e1de418e21b
Ron MacNeil <macro@hotmail.com>
2014-06-07 20:38:28 +1000

    Update copyright

--- makery-srcs-preprocess/rules.mk
+++ makery-srcs-preprocess/rules.mk
@@ -1,6 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright (c) 2010, 2011
-# Ron MacNeil <macro@hotmail.com>
+# Copyright (c) Ron MacNeil <macro@hotmail.com>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
--- makery-system/global.mk
+++ makery-system/global.mk
@@ -1,6 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013
-# Ron MacNeil <macro@hotmail.com>
+# Copyright (c) Ron MacNeil <macro@hotmail.com>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
