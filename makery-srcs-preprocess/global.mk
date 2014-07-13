b045ade1fe577e82a14054ce30916062b7659466
Ron MacNeil <macro@hotmail.com>
2014-07-13 21:42:12 +1000

    srcs-preprocess: Add functions to get info from upstream preprocessor modules

---
+++ makery-srcs-preprocess/global.mk
@@ -0,0 +1,51 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Ron MacNeil <macro@hotmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# ------------------------------------------------------------------------------
+
+
+# Get the name of the module currently at the end of the preprocessor pipeline
+#
+SRCS_PREPROCESS_GetUpstream = \
+$(lastword $(SRCS_PREPROCESS_pipeline))
+
+
+# Get a preprocessor module's output root source code directory
+#
+# $1 - Preprocessor module name
+#      default: SRCS_PREPROCESS_GetUpstream()
+#
+SRCS_PREPROCESS_GetDir = \
+$(or $($(call MODULES_VariablePrefix,$(or $(1),$(call SRCS_PREPROCESS_GetUpstream)))_dir),$($(call MODULES_VariablePrefix,$(or $(1),$(call SRCS_PREPROCESS_GetUpstream)))_outdir))
+
+
+# Get a preprocessor module's output source code files relative to
+# SRCS_PREPROCESS_GetDir()
+#
+# $1 - Preprocessor module name
+#      default: SRCS_PREPROCESS_GetUpstream()
+#
+SRCS_PREPROCESS_GetFiles = \
+$(or $($(call MODULES_VariablePrefix,$(or $(1),$(call SRCS_PREPROCESS_GetUpstream)))_rel),$(call MAKE_Shell,cd $(call SYSTEM_ShellEscape,$(call SRCS_PREPROCESS_GetDir,$(1))) && find * -type f | $(SYSTEM_SHELL_CLEANPATH) | $(SYSTEM_SHELL_ENCODEWORD)))
+
+
+# Get prerequisites representing a preprocessor module's output source code
+# files
+#
+# $1 - Preprocessor module name
+#      default: SRCS_PREPROCESS_GetUpstream()
+#
+SRCS_PREPROCESS_GetPreqs = \
+$(or $($(call MODULES_VariablePrefix,$(or $(1),$(call SRCS_PREPROCESS_GetUpstream)))_preq),$(call MAKE_EncodeWord,$($(call MODULES_VariablePrefix,$(or $(1),$(call SRCS_PREPROCESS_GetUpstream)))_dotfile)))
+
--- makery-srcs-preprocess/project.mk
+++ makery-srcs-preprocess/project.mk
@@ -15,50 +15,17 @@
 # ------------------------------------------------------------------------------
 
 
-#
-# A preprocessor module must implement variables according to the following
-# patten (<PP> is the prefix that the module adds to _pipeline):
-#
-# <PP>_dir      - Common root directory of source code files
-# <PP>_rel      - Source code files relative to <PP>_dir
-#               - Assumed to be target-time
-# <PP>_preq     - Files that represent the source code files for prerequisites
-#                 purposes (absolute paths)
-#
 SRCS_PREPROCESS_pipeline_DESC := \
-Preprocessor list (append-only list)
+Preprocessor pipeline module names (append-only list)
 $(call PROJ_DeclareVar,SRCS_PREPROCESS_pipeline)
 SRCS_PREPROCESS_pipeline_VALIDATE = Required
 
 
-SRCS_PREPROCESS_ppfrom_DESC := \
-Upstream processor in preprocessor pipeline
-$(call PROJ_DeclareVar,SRCS_PREPROCESS_ppfrom)
-SRCS_PREPROCESS_ppfrom_DEFAULT = $(lastword $(SRCS_PREPROCESS_pipeline))
-
-
-SRCS_PREPROCESS_srcdir_DESC := \
-(read-only) Source code files root directory
-$(call PROJ_DeclareVar,SRCS_PREPROCESS_srcdir)
-SRCS_PREPROCESS_srcdir_DEFAULT = $($(SRCS_PREPROCESS_ppfrom)_dir)
-
-
-SRCS_PREPROCESS_srcs_DESC := \
-(read-only) Source code files relative to SRCS_PREPROCESS_srcdir
-$(call PROJ_DeclareTargetVar,SRCS_PREPROCESS_srcs)
-SRCS_PREPROCESS_srcs = $($(SRCS_PREPROCESS_ppfrom)_rel)
-
-
-SRCS_PREPROCESS_srcpreq_DESC := \
-(read-only) Source code files prerequisite files
-$(call PROJ_DeclareVar,SRCS_PREPROCESS_srcpreq)
-SRCS_PREPROCESS_srcpreq_DEFAULT = $($(SRCS_PREPROCESS_ppfrom)_preq)
-
-
-# Hook the end of the pipeline to SRCS_*
+# Hook the end of the pipeline to makery-srcs
 #
 SRCS_files = \
-$(foreach f,$(SRCS_PREPROCESS_srcs),$(call MAKE_EncodeWord,$(SRCS_PREPROCESS_srcdir))/$(f))
+$(foreach f,$(call SRCS_PREPROCESS_GetFiles),$(call MAKE_EncodeWord,$(call SRCS_PREPROCESS_GetDir))/$(f))
 
-SRCS_preqs = $(SRCS_PREPROCESS_srcpreq)
+SRCS_preqs = \
+$(call SRCS_PREPROCESS_GetPreqs)
 
