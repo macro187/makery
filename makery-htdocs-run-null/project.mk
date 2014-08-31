9d7ae8f4e6a3e25303db9f71f2fd3afcece4d24a
Ron MacNeil <macro@hotmail.com>
2014-08-31 11:53:59 +1000

    htdocs-run-null: Improve messaging

--- makery-htdocs-run-null/project.mk
+++ makery-htdocs-run-null/project.mk
@@ -20,6 +20,5 @@ HTDOCS_RUN_NULL_run_DESC ?= \
 Command to (pretend to) serve htdocs
 $(call PROJ_DeclareVar,HTDOCS_RUN_NULL_run)
 HTDOCS_RUN_NULL_run = \
-@echo If a real dev web server was available, this is where it would start serving \'$(HTDOCS_outdir)\' on port \'$(HTDOCS_RUN_port)\'
-
+echo && echo ===\> If a dev web server was available, this is where it would start serving && echo ===\> \'$(HTDOCS_outdir)\' && echo ===\> on port \'$(HTDOCS_RUN_port)\' && echo
 
